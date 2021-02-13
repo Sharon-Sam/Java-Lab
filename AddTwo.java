48bc73abc9acf03fdbf39f47656b5539cbb48da5
Sharon Sam <72041910+Sharon-Sam@users.noreply.github.com>
2021-02-13 10:52:21 +0530

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,12 +1,20 @@
 //to display sum of 2 numbers
 
+import java.util.Scanner;
 class AddTwo{
 	public static void main(String args[]){
-		int n=9;
-		int m=10;
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter the first number:");
+		int n =obj.nextInt();
+		System.out.println("Enter the second number:");
+		int m= obj.nextInt();
 		System.out.println("Sum="+(n+m));
 	}
 }
 
 /*OUTPUT
-Sum=19*/
+Enter the first number:
+3
+Enter the second number:
+5
+sum=8 */
