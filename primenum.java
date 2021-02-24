52776eb01cc68494767403767e882202b6143328
Sharon Sam <72041910+Sharon-Sam@users.noreply.github.com>
2021-02-24 14:35:39 +0530

    Update primenum.java

--- primenum.java
+++ primenum.java
@@ -7,16 +7,16 @@ class primenum{
 		if(n==0 || n==1){
 			System.out.println("Not a prime number");
  		}
-		for(int i=1;i<=n;i++){	
+		for(int i=2;i<=n/2;i++){	
 			if(n%i==0){
 				count ++;
 			}
 		}
-		if(count==2){
-			System.out.println(n+ " is a prime number");
+		if(count==1){
+			System.out.println(n+ " is not a prime number");
 			}
 		else{
-			System.out.println(n+ " is not a prime number");
+			System.out.println(n+ " is a prime number");
 		}
 	}
 }
